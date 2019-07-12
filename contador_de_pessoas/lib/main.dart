1c0cf82574a0e632aa5f5860745fdd54098b96bc
tofarias <tiago.farias.poa@gmail.com>
2019-07-12 10:20:05 -0300

    Add textos e alinhamento

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( MaterialApp(
-    title: "Contador de Pessoas",
-    home: Container(color: Colors.white)
-  ));
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)
+          ),
+          Text("Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0)
+          )
+        ],
+      )));
 }
